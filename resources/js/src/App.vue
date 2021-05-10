6d0abe832c21832836067f568e92010f9c09675f
Mohammed Motar <mohammed.motar.mm@gmail.com>
2021-05-10 16:42:32 +0200

    app

---
+++ resources/js/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <!-- <v-main class="mb-4 mx-auto" style="width: 100%">
+      <router-view />
+    </v-main> -->
+    <div>hej</div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+</style>
